8935796238ac30a5f967528a965258e01e59c731
TaalibAsaf <137051454+TaalibAsaf@users.noreply.github.com>
2024-04-14 21:59:18 +0530

    updated anr

--- src/pages/AnnualReport.jsx
+++ src/pages/AnnualReport.jsx
@@ -1,32 +1,49 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import PageTitle from '../components/pagetitle/PageTitle';
-import About1 from '../components/about/About1';
-import Partner from '../components/partner/Partner';
 import Footer2 from '../components/footer/Footer2';
-import dataAbout from '../assets/fake-data/data-about';
-import dataPartner from '../assets/fake-data/data-partner';
-import Team2 from '../components/team/Team2';
-import dataTeam from '../assets/fake-data/data-team';
 import ARItem from '../components/annual_report/AR_item';
-
-const data = [
-    {
-        title : "hello",
-
-}
-] 
+import client from '../services/client'
+import Loading from '../components/loader/loader';
 
 function AnnualReport(props) {
+    const [annualRep, setAnnualRep] = useState(null);
+    useEffect(() => {
+        client.fetch(`*[_type == 'annualReport' && showReport == true ] {
+            title,
+            slug,
+            publishedAt,
+            "reportFile": upload.file.asset->url,
+            showReport,
+            mainImage{
+                asset->{
+                        _id,
+                        url
+                    },
+                    alt
+                      }
+          }`)
+        .then((data) => {
+            setAnnualRep(data);
+            console.log(data);
+        })
+        .catch(console.error)
+    }, []); 
+
+
+    if(annualRep === null){
+        return(<Loading/>);
+    }
     return (
         <div className='page-about home-1'>
             <PageTitle title='Annual Report' />
-            <ARItem/>
-            <ARItem/>
-            <ARItem/>
-            <ARItem/>
-            <ARItem/>
-            <ARItem/>
-            <ARItem/>
+            {annualRep.map((item,idx) => (  
+                <ARItem
+                title= {item.title}
+                reportFile ={item.reportFile}
+                slug={item.slug.current}
+                image={item.mainImage.asset.url}
+                />
+              ))}
             <Footer2 />
 
             
