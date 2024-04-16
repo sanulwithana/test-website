e7eea1af66dda4c13983e63c1ce7da417a6c552f
TaalibAsaf <137051454+TaalibAsaf@users.noreply.github.com>
2024-04-16 12:56:45 +0530

    seto the award layout

--- src/pages/Awards.jsx
+++ src/pages/Awards.jsx
@@ -15,32 +15,9 @@ function Awards(props) {
             <PageTitle title='Awards' />
 
             <section className="tf-collection-inner">
-                <div className="tf-container">
-                    <div className="row ">
-                        <div className="col-lg-3 col-md-4">
-                        </div>
-                        <div className="col-lg-9 col-md-8 ">
-                            <div className="row">
-                                {
-                                    data.slice(0,9).map(idx => (
-                                        <div key={idx.id} className="col-lg-4 col-md-6 col-sm-6 col-12 ">
-                                            <div className="tf-product">
-                                                <div className="image">
-                                                    <img src={idx.img} alt="Binabox" />
-                                                </div>
-                                                <h6 className="name"><Link to="/item-details">{idx.title}</Link></h6>
-                                            </div>
-                                        </div>
-                                    ))
-                                }
-
-
-                            </div>
-                             
-                        </div>
-                    </div>
-                    
-                </div>
+                {AwardItem()}
+                {AwardItem()}
+                {AwardItem()}
             </section>
                 
             <Footer />
@@ -50,3 +27,34 @@ function Awards(props) {
 }
 
 export default Awards;
+
+export function AwardItem() {
+    return <div className="tf-container">
+        <div className="container" style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+            <h2 className="heading">2023/24</h2>
+        </div>
+
+        <div className="row ">
+            <div className="col-lg-2 col-md-2">
+            </div>
+            <div className="col-lg-9 col-md-8 ">
+                <div className="row">
+                    {data.map(idx => (
+                        <div key={idx.id} className="col-lg-3 col-md-1">
+                            <div className="tf-product">
+                                <div className="image" style={{ marginBottom: '4rem' }}>
+                                    <img src={idx.img} alt="Binabox" />
+                                </div>
+
+                            </div>
+                        </div>
+                    ))}
+
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>;
+}
