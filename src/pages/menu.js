1f8888bcb614b316ad18b1adedc5f207c697f156
TaalibAsaf <137051454+TaalibAsaf@users.noreply.github.com>
2024-04-21 04:46:55 +0530

    updated teh sun button

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -65,58 +65,6 @@ const menus = [
             },
         ]
     },
-    {
-        id: 3,
-        name: 'Avenues',
-        links: '#',
-        namesub: [
-            {
-                id: 2,
-                sub: 'Sign up',
-                links: '/signup'
-            },
-            {
-                id: 3,
-                sub: 'Faq v1',
-                links: '/faq-v1'
-            },
-            {
-                id: 4,
-                sub: 'Faq v2',
-                links: '/faq-v2'
-            },
-            {
-                id: 5,
-                sub: 'Our Team',
-                links: '/our-team'
-            },
-            {
-                id: 6,
-                sub: 'Collection',
-                links: '/collection'
-            },
-            {
-                id: 7,
-                sub: 'Testimonial',
-                links: '/testimonial'
-            },
-            {
-                id: 8,
-                sub: 'Item Details',
-                links: '/item-details'
-            },
-            {
-                id: 9,
-                sub: 'Comming Soon',
-                links: '/comming-soon'
-            },
-            {
-                id: 10,
-                sub: 'Page 404',
-                links: '/page-404'
-            },
-        ],
-    },
 
     {
         id: 5,
